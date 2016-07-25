1ed5c910a652a9219d1f861d2dea880100e8762f
fatalerrortan <tiemann9898@gmail.com>
2016-07-25 21:53:32 +0200

    email function and redirect done

---
+++ email.php
@@ -0,0 +1,4 @@
+<?php
+$test = "Bitte Klicken Sie das link zur Bestätigung Ihrer Registrierung bei uns! => http://www.xulin-tan.de/iems/tarif.php";
+mail('tiemann9898@gmail.com', 'Bestätigungsemail', $test);
+?>
